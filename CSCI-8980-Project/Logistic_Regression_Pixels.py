3ddccc814c359d01ceb7a507fd4d3ac462c2377b
Akhil Bhargava <bharg016@umn.edu>
2018-02-03 18:56:27 -0600

    Updated Code and Report

--- CSCI-8980-Project/Logistic_Regression_Pixels.py
+++ CSCI-8980-Project/Logistic_Regression_Pixels.py
@@ -8,10 +8,16 @@ import matplotlib.pyplot as plt
 import urllib.request
 get_ipython().magic('matplotlib inline')
 import pandas as pd
+import seaborn as sns
+from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+from sklearn import metrics
 from sklearn import preprocessing
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_curve, auc
+
 
 
 #loading data from the server. This will download the dataset in your curernt directory and then load it.
@@ -32,12 +38,50 @@ X.columns = ['X1','X2','X3','X4','X5','X6','X7']
 Y = pd.DataFrame.from_dict(data['Y_all'])
 Y.columns = ['Label']
 
+#Transform Y into enoded
+label_encoder= preprocessing.LabelEncoder()
+Y = label_encoder.fit_transform(Y)
+
+#PCA to see explained variance of component
+pca = PCA()
+pca.fit_transform(X)
+explained_variance_ratio = pca.explained_variance_ratio_
+cumalative_explained_variance = np.cumsum(explained_variance_ratio)
+
+#Plot PCA componenents
+with plt.style.context('seaborn-whitegrid'):
+    plt.bar(range(7), explained_variance_ratio, align = 'center', alpha = 0.5, label = 'explained variance bar plot')
+    plt.step(range(7), cumalative_explained_variance, where = 'mid', label = 'cumalative explained variance')
+    plt.ylabel('Explained Variance')
+    plt.xlabel('Principal Component')
+    plt.legend(loc = 'best')
+
 #Split into training and testing
 X_train, X_test, y_train, y_test = train_test_split(X,Y,random_state = 0)
-classifier = LogisticRegression(random_state=0)
-classifier.fit(X_train, np.ravel(y_train))
-y_pred = classifier.predict(X_test)
-print(classification_report(y_test,y_pred))
-print ( classifier.score(X_test, y_test))
 
-print(classifier.coef_)
+#Simple Logistic Regression
+model_LR = LogisticRegression(random_state=0)
+model_LR.fit(X_train, np.ravel(y_train))
+y_prob = model_LR.predict_proba(X_test)[:,1]
+y_pred = model_LR.predict(X_test)
+
+
+print(metrics.confusion_matrix(y_test,y_pred))
+print ( model_LR.score(X_test, y_test))
+
+auc_roc=metrics.roc_auc_score(y_test,y_pred)
+auc_roc
+
+false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_prob)
+roc_auc = auc(false_positive_rate, true_positive_rate)
+roc_auc
+
+plt.figure(figsize=(5,5))
+plt.title('Receiver Operating Characteristic')
+plt.plot(false_positive_rate,true_positive_rate, color='red',label = 'AUC = %0.3f' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],linestyle='--')
+plt.axis('tight')
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+
--- CSCI-8980-Project/ST_DataMining_Project.py
+++ CSCI-8980-Project/ST_DataMining_Project.py
@@ -10,49 +10,17 @@ get_ipython().magic('matplotlib inline')
 import scipy.stats
 import tensorflow as ts
 import keras
-from keras.layers import Dense, Reshape, Flatten,Conv2D, MaxPooling2D,Conv2DTranspose, Activation, Dropout, BatchNormalization
+from matplotlib import colors
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.layers import Input, concatenate, Dense, Reshape, Flatten,Conv2D, MaxPooling2D,Conv2DTranspose, Activation, Dropout, BatchNormalization
 from keras.models import Sequential
 from sklearn import preprocessing
+from keras.callbacks import History 
 
 
-#loading data from the server. This will download the dataset in your current directory and then load it.
-dataset_name = 'ImageLevelDataset_Version2'
-url = 'http://umnlcc.cs.umn.edu/WaterDatasets/' + dataset_name + '.zip'
-urllib.request.urlretrieve(url,dataset_name + '.zip')
-os.system('unzip ' + dataset_name + '.zip')
-print('Dataset Loaded ...')
-
-#Get image_names and id names
-image_names = os.listdir(dataset_name)
-ID_name = []
-for image_file in image_names:
-    ID_name.append(image_file.replace('data_','').replace('.mat',''))
-
-# Visualize a lake image
-ID = ID_name[811]
-data = matlab.loadmat(dataset_name + '/data_' + ID +'.mat')
-X = data['X'].astype(np.float64)
-min_val = 0
-max_val = np.amax(X)
-X[X[:, :, :] > max_val] = max_val
-X[X[:, :, :] < min_val] = min_val
-
-for b in range(X.shape[2]):
-    X[:, :, b] = X[:, :, b] * 2.0 / (max_val - min_val)
-
-
-plt.figure(figsize = (15,8))
-plt.subplot(1,2,1)
-plt.imshow(X[:,:,[0, 3, 2]])
-plt.title('Color Composite Image')
-plt.subplot(1,2,2)
-plt.imshow(data['Y'])
-plt.title('Class Label Map')
-plt.show()
-
-
-#Function to create model
-def model_build(train_X):
+#Function to create Fully Convolutional Network
+def FCN_build(train_X):
     
     model = Sequential()
     model.add(Conv2D(64, kernel_size=(3, 3),
@@ -65,16 +33,62 @@ def model_build(train_X):
     model.add(Conv2DTranspose(112,(4,4), activation ='relu'))
     model.add(Conv2DTranspose(64,(3,3), activation = 'relu'))
     model.add(Conv2DTranspose(3,(2,2), activation = 'relu'))
-    model.add(Reshape(((train_X.shape[1] ** 2),3)))
-    model.add(Activation('softmax'))
-  
+    model.add(Reshape(((train_X.shape[1] * train_X.shape[2]),3)))
+    model.add(Activation('softmax') )
+    
 
+    model.compile(loss = 'categorical_crossentropy', optimizer= 'adam', metrics=['accuracy'])
     
-    
-    model.compile(loss= 'categorical_crossentropy',
-                  optimizer= 'adam',
-                  metrics=['accuracy'])
     return(model)
+
+
+
+def UNET_build(train_X):
+    inputs = Input((train_X.shape[1], train_X.shape[2], train_X.shape[3]))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(112, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(112, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+    conv6 = Conv2D(128, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+    conv7 = Conv2D(112, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(112, (3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(3, (1, 1), activation='sigmoid')(conv9)
+    reshape11 = Reshape(((train_X.shape[1] * train_X.shape[2]),3))(conv10)
+
+    model = Model(inputs=[inputs], outputs=[reshape11])
+
+    model.compile(optimizer=Adam(lr=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
+
+
+    return model
 
 #Function to get largest image
 
@@ -92,7 +106,7 @@ def smallest_image(IDs):
 def Patch_creator(image, X_array, Y_array, horizontal, vertical):
     
     #Get Features (pixels) and labels for the image
-    pixels = image['X'][:,:,(0,3,2)]
+    pixels = image['X']
     labels = image['Y']
     
     #Used to encode
@@ -119,6 +133,7 @@ def Patch_creator(image, X_array, Y_array, horizontal, vertical):
             #\Get all horizontal shifts for each vertical shift first
             for horizontal_shift in range(Num_Horizontal):
                 
+                
                 horz_pos1 = horz_pos1 + horizontal
                 horz_pos2 = horz_pos2 + horizontal
                 
@@ -135,17 +150,18 @@ def Patch_creator(image, X_array, Y_array, horizontal, vertical):
                 Encoded_Y_patch = Encoded_Y_patch.reshape(
                         1,Encoded_Y_patch.shape[0], Encoded_Y_patch.shape[1])
                 
-                #If you have an empty array, create a new one,
-                #Else keep adding patches and info
-                if X_array == []:
-                    X_array = np.array(X_patch)
-                elif X_array != []:
-                    X_array = np.vstack( (X_array,X_patch))
+                if sum(Encoded_Y_patch[0,:])[1] != Encoded_Y_patch.shape[1] and sum(Encoded_Y_patch[0,:])[0] != Encoded_Y_patch.shape[1] and sum(Encoded_Y_patch[0,:])[2] != Encoded_Y_patch.shape[1]:
+                    #If you have an empty array, create a new one,
+                    #Else keep adding patches and info
+                    if X_array == []:
+                        X_array = np.array(X_patch)
+                    elif X_array != []:
+                        X_array = np.vstack( (X_array,X_patch))
                 
-                if Y_array == []:
-                    Y_array = np.array(Encoded_Y_patch)
-                elif Y_array != []:
-                    Y_array = np.vstack( (Y_array,Encoded_Y_patch))
+                    if Y_array == []:
+                        Y_array = np.array(Encoded_Y_patch)
+                    elif Y_array != []:
+                        Y_array = np.vstack( (Y_array,Encoded_Y_patch))
             
             #Shift vertical Patch
             vert_pos1 = vert_pos1 + vertical
@@ -157,23 +173,40 @@ def Patch_creator(image, X_array, Y_array, horizontal, vertical):
         
     return (X_array, Y_array)
 
-
-def Visualize_Patch(Patch_Index,test_Y, predicted_Y, horizontal, vertical):
+#function to display results
+def Visualize_Patch(Patch_Index,test_X, test_Y, predicted_Y, horizontal, vertical):
     
     #Get the patches
     realpatch = test_Y[Patch_Index,:]
     predictedpatch = predicted_Y[Patch_Index,:]
+    X = test_X[Patch_Index,:].astype(np.float64)
 
     #Reshape the patches to have the max probability of the class in the original size
     predictedpatch = np.asarray(pd.DataFrame(predictedpatch).idxmax(1)).reshape(vertical-1,horizontal-1).astype(int)
     realpatch = np.asarray(pd.DataFrame(realpatch).idxmax(1) ).reshape(vertical-1,horizontal-1).astype(int)
     
+    #Get X to 0 - 1 Range
+    min_val = 0
+    max_val = np.amax(X)
+    X[X[:, :, :] > max_val] = max_val
+    X[X[:, :, :] < min_val] = min_val
+
+    for b in range(X.shape[2]):
+        X[:, :, b] = X[:, :, b] * 2.0 / (max_val - min_val)
+    
+    #define color map
+    cmap_dataset1 = colors.ListedColormap(['darkblue', 'green','white'])
+    cmap_dataset2 = colors.ListedColormap(['darkblue','green'])
+
     #plot
-    plt.subplot(1,2,1)
-    plt.imshow(predictedpatch)
+    plt.subplot(1,3,1)
+    plt.imshow(X[:,:,[0,3,2]],)
+    plt.title('RGB Visualization')
+    plt.subplot(1,3,2)
+    plt.imshow(predictedpatch, cmap = cmap_dataset2)
     plt.title('Predicted Values')
-    plt.subplot(1,2,2)
-    plt.imshow(realpatch)
+    plt.subplot(1,3,3)
+    plt.imshow(realpatch,cmap=cmap_dataset2)
     plt.title('Real Values')
 
 def get_Land_perc(test_Y):
@@ -182,48 +215,177 @@ def get_Land_perc(test_Y):
         land_count = land_count + sum(np.asarray(pd.DataFrame(test_Y[patch_num,:]).idxmax(1)) == 1)
     
     return(land_count / (test_Y.shape[0] * test_Y.shape[1]))
-
     
-#Initalize and create Patches
-train_X = train_Y = test_Y = test_X = valid_X = valid_Y = []
-train_set = ID_name[0:700]
-valid_set = ID_name[701:810]
-test_set = ID_name[811:955]
-
-vertical = 17
-horizontal = 17
-
-for ID in train_set:
+# Visualize a lake image
+def Visualize_Image(ID_index, ID_name):
+    ID = ID_name[ID_index]
     data = matlab.loadmat(dataset_name + '/data_' + ID +'.mat')
-    train_X,train_Y = Patch_creator(data, train_X, train_Y, vertical, horizontal )
+    X = data['X'].astype(np.float64)
+    min_val = 0
+    max_val = np.amax(X)
+    X[X[:, :, :] > max_val] = max_val
+    X[X[:, :, :] < min_val] = min_val
+
+    for b in range(X.shape[2]):
+        X[:, :, b] = X[:, :, b] * 2.0 / (max_val - min_val)
+
+    plt.figure(figsize = (8,8))
+    plt.subplot(1,2,1)
+    plt.imshow(X[:,:,[0, 3, 2]],interpolation='nearest')
+    plt.title('Color Composite Image')
+    plt.subplot(1,2,2)
+    plt.imshow(data['Y'],interpolation='nearest',cmap = colors.ListedColormap(['darkblue','green']))
+    plt.title('Class Label Map')
+    plt.show()
     
-for ID in valid_set:
-    data = matlab.loadmat(dataset_name + '/data_' + ID +'.mat')
-    valid_X, valid_Y = Patch_creator(data, valid_X, valid_Y, vertical, horizontal)
+def metrics(predicted_labels, true_labels):
+    #Intalize values
+    #TP = correctly classify pixel as land
+    #TN = correctly classify pixel as water
+    #FP = incorrectly classify pixel as land
+    #FN = incorrectly classify pixel as water
     
-for ID in test_set:
-    data = matlab.loadmat(dataset_name + '/data_' + ID +'.mat')
-    test_X, test_Y = Patch_creator(data,test_X,test_Y,vertical, horizontal)
+    FP = TP = TN = FN = 0
+    
+    #Create a dataframe for each patch
+    for patch in range(predicted_labels.shape[0]):
+        pred_df = pd.DataFrame(predicted_labels[patch,:]).idxmax(1)
+        true_df = pd.DataFrame(true_labels[patch,:]).idxmax(1)
+        
+        #go through each row
+        for row in range(pred_df.shape[0]):
+            
+            #If prediction is land
+            if pred_df[row] == 1:
+                if true_df[row] == 1:
+                    TN += 1
+                elif true_df[row] == 0:
+                    FN += 1
+                    
+            #If prediction is water
+            elif pred_df[row] == 0:
+                if true_df[row] == 0:
+                    TP += 1
+                elif true_df[row] == 1:
+                    FP += 1
+    
+    Precision = TP/ (TP + FP)
+    Recall = TP/(TP + FN)
+    F1_score = (2 * Recall * Precision) / (Recall + Precision)
+    Accuracy = (TN + TP)/(TP + TN + FP + FN)
+    return(F1_score,Accuracy,TP,FP,FN,TN)
     
 
-#Build Model
-model = model_build(train_X)
+if __name__ == '__main__':
+    
+    #loading data from the server. This will download the dataset in your current directory and then load it.
+    dataset_name = 'ImageLevelDataset_Version2'
+    #url = 'http://umnlcc.cs.umn.edu/WaterDatasets/' + dataset_name + '.zip'
+    #urllib.request.urlretrieve(url,dataset_name + '.zip')
+    os.system('unzip ' + dataset_name + '.zip')
+    print('Dataset Loaded ...')
 
-#Train Model with Validation Set
-model.fit(train_X, train_Y,
+    #Get image_names and id names
+    image_names = os.listdir(dataset_name)
+    ID_name = []
+    for image_file in image_names:
+        ID_name.append(image_file.replace('data_','').replace('.mat',''))
+
+    
+    #Initalize and create Patches
+    train_X = train_Y = test_Y = test_X = valid_X = valid_Y = []
+    train_set = ID_name[0:700]
+    valid_set = ID_name[701:810]
+    test_set = ID_name[811:955]
+
+    vertical = 33    
+    horizontal = 33
+
+    for ID in train_set:
+        data = matlab.loadmat(dataset_name + '/data_' + ID +'.mat')
+        train_X,train_Y = Patch_creator(data, train_X, train_Y, vertical, horizontal )
+    
+    for ID in valid_set:
+        data = matlab.loadmat(dataset_name + '/data_' + ID +'.mat')
+        valid_X, valid_Y = Patch_creator(data, valid_X, valid_Y, vertical, horizontal)
+    
+    for ID in test_set:
+        data = matlab.loadmat(dataset_name + '/data_' + ID +'.mat')
+        test_X, test_Y = Patch_creator(data,test_X,test_Y,vertical, horizontal)
+    
+
+    #Create Models
+    
+    history = History()
+    
+   #UNET only works for size 16 * 16 images or higher
+    if (vertical -1 ) % 16 == 0 and (horizontal -1)  % 16 == 0:
+        
+        #Build UNET
+        UNET = UNET_build(train_X)
+        UNET.summary()
+        
+        #Train UNET
+        UNET.fit(train_X, train_Y,
           batch_size= 16,
-          epochs= 10,
+          callbacks = [history],
+          epochs= 1,
           verbose=1,
           validation_data=(valid_X, valid_Y ))
-
-#Evaluate Model
-print(model.evaluate(test_X, test_Y))
-
-predicted_values = model.predict(test_X)
-
-Visualize_Patch(0,test_Y,predicted_values, vertical, horizontal)
-
-
+        
+        #Predict Using UNET
+        UNET_predicted_values = UNET.predict(test_X)
+        
+        #Get Metrics for UNET
+        (F1,Accuracy, TP, FP, FN, TN) = metrics(UNET_predicted_values,test_Y)
+        print("metrics for UNET:\nThe accuracy is %f \nThe F1 Score is %f \nThe number of pixels correctly classified as land is %d \nThe number of pixels correctly classified as water is %d \nThe number of pixels incorrectly classified as land is %d \nThe number of pixels incorrectly classified as water is %d"  % (Accuracy,F1, TN, TP, FN, FP) )
+        
+        #Visualize patch for UNET
+        #Visualize_Patch(43,test_X,test_Y,UNET_predicted_values, vertical, horizontal)
     
-land_percent = get_Land_perc(test_Y)
-print(land_percent)
+
+    #Fit and Train FCN
+    FCN = FCN_build(train_X)
+    FCN.summary()    
+    history = FCN.fit(train_X, train_Y,
+          batch_size= 16,epochs= 30,
+          verbose=1, callbacks = [history],
+          validation_data=(valid_X, valid_Y ))
+
+    #Generate FCN Predictions
+    FCN_predicted_values = FCN.predict(test_X)
+    
+    #Get Metrics for FCN
+    (F1, Accuracy, TP, FP, FN, TN) = metrics(FCN_predicted_values,test_Y)
+    print("metrics for FCN:\nThe accuracy is %f \nThe F1 Score is %f \nThe number of pixels correctly classified as land is %d \nThe number of pixels correctly classified as water is %d \nThe number of pixels incorrectly classified as land is %d \nThe number of pixels incorrectly classified as water is %d"  % (Accuracy,F1, TN, TP, FN, FP) )  
+   
+    #Visualize Patch
+    Visualize_Patch(65,test_X,test_Y,FCN_predicted_values, vertical, horizontal)
+
+    land_percent = get_Land_perc(test_Y)
+    print("Land percent is: %f"  % land_percent )
+    
+    plt
+    #Predict patches for missing values in ImageDataSet1
+    
+    dataset_name = 'ImageLevelDataset_Version1'
+    #url = 'http://umnlcc.cs.umn.edu/WaterDatasets/' + dataset_name + '.zip'
+    #urllib.request.urlretrieve(url,dataset_name + '.zip')
+    os.system('unzip ' + dataset_name + '.zip')
+    print('Generating Patches for ImageLevelDataset_Version1...')
+
+    #Get image_names and id names
+    image_names = os.listdir(dataset_name)
+    ID_name = []
+    Dataset1_X = Dataset1_Y = []
+    for image_file in image_names:
+        ID_name.append(image_file.replace('data_','').replace('.mat',''))
+    
+    for ID in ID_name[0:15]:
+        data = matlab.loadmat(dataset_name + '/data_' + ID +'.mat')
+        Dataset1_X,Dataset1_Y = Patch_creator(data, Dataset1_X, Dataset1_Y, vertical, horizontal )
+    
+    #Make predictions with FCN for ImageLevelDataset_1
+    Image1_pred_values = FCN.predict(Dataset1_X)
+    Visualize_Patch(200,Dataset1_X,Dataset1_Y,Image1_pred_values,vertical,horizontal)
+        
